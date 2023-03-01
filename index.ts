fc683bb0878300b82fe1f666fb7305b8a0b6a9d1
ziyou <874928519@qq.com>
2023-03-01 10:35:10 +0800

    feat: 处理配置文件

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 import staticPic from "./assets/static.png";
+import "./assets/index.css";
 const staticDom = document.createElement("div");
-staticDom.setAttribute("class", "static-box_date87234");
 staticDom.innerHTML = `<img src="${staticPic}" alt="">`;
 
+const defaultProps = {
+  position: "right-bottom",
+  cssText:
+    "display:inline-block; width: 285px; height: 80px; background: rgba(48, 65, 86, 0.2); position: absolute;z-index: 99",
+};
 export default {
-  mounted(el: HTMLElement) {
+  mounted(el, { value = defaultProps }) {
+    const { position, cssText } = value;
     el.style.position = "relative";
+    staticDom.setAttribute("class", `static-box_date87234 ${position}`);
+    staticDom.style.cssText = cssText;
     el.appendChild(staticDom);
   },
 };
