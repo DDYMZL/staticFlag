cfb0bfa9836f3f6db6bb6172a009da96b3e3dd0d
ziyou <874928519@qq.com>
2023-03-01 16:16:20 +0800

    feat: 初步完成v2/v3兼容

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { Vue } from "vue-demi";
 import staticPic from "./assets/static.png";
 import "./assets/index.css";
 const staticDom = document.createElement("div");
@@ -8,7 +9,8 @@ const defaultProps = {
   cssText:
     "display:inline-block; width: 285px; height: 80px; background: rgba(48, 65, 86, 0.2); position: absolute;z-index: 99",
 };
-export default {
+const isV3 = Object.keys(Vue).length > 10;
+const v3 = {
   mounted(el, { value = defaultProps }) {
     const { position, cssText } = value;
     el.style.position = "relative";
@@ -17,3 +19,16 @@ export default {
     el.appendChild(staticDom);
   },
 };
+
+const v2 = {
+  bind(el, binding = defaultProps) {
+    console.log(binding);
+    const { position, cssText } = binding;
+    el.style.position = "relative";
+    staticDom.setAttribute("class", `static-box_date87234 ${position}`);
+    staticDom.style.cssText = cssText;
+    el.appendChild(staticDom);
+  },
+};
+
+export default isV3 ? v3 : v2;
